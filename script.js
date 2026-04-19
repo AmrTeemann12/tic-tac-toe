4a3cbf0148f10a8efde084df2241b616ff110cb6
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-04-19 16:21:31 +0200

    add players object and the object for managing a round. added explicit true and false return value for the the assign mark function in the gameBoard object. added clearing the tracker's arrays in the clearBoard method. add the factory function for creating a player. created two players A and B. wrote the logic for playing a round into the playRound object.

--- script.js
+++ script.js
@@ -37,11 +37,12 @@ const gameBoard = (function (){
         const slot = board[row][col];
         if(slot.mark !==null){
             console.warn('the slot is already marked');
-            return;
+            return false;
         }
-        board[row][col].mark = mark;
+        slot.mark = mark;
         
         tracker[mark].push(`${row}-${col}`)
+        return true;
     }
 
     function checkWinningPattern (mark){
@@ -56,10 +57,75 @@ const gameBoard = (function (){
                 slot.mark = null;
             })
         })
+        tracker.x.length = 0;
+        tracker.o.length = 0;
     }
 
     const getBoard = () => board;
     const getTracker = () => tracker;
 
     return {assignMark, getBoard, getTracker, checkWinningPattern, resetBoard};
-})()
+})();
+
+function createPlayer(name, mark, score){
+    const addPoint = () => {
+        score++
+    }
+
+    const getScore = () => score;
+    const toggleMark = () => {
+        if(mark === 'x'){
+            mark = 'o';
+        } else if (mark === 'o'){
+            mark = 'x';
+        }
+    };
+    const getMark = () => mark;
+
+    return {name, toggleMark, getMark, addPoint, getScore};
+}
+
+const playerA = createPlayer("A", "x", 0);
+const playerB = createPlayer("B", "o", 0);
+
+const playRound = (function(playerA, playerB, board){
+    const xPlayer = () => playerA.getMark() === 'x'? playerA : playerB; 
+    const oPlayer = () => playerA.getMark() === 'o'? playerA : playerB;
+    let roundCount = 0;
+    let playerWin = false;
+
+    function applyMark (row, col){
+        if (checkRoundEnd()) newRound();
+        let currentPlayer = roundCount % 2 === 0? xPlayer() : oPlayer();
+        const assigned = board.assignMark(currentPlayer.getMark(), row, col)
+            
+        if(assigned){
+            roundCount++;
+            checkRoundResult(currentPlayer);
+        }
+    }
+
+    function checkRoundResult (player){
+        if(board.checkWinningPattern(player.getMark())){
+            player.addPoint();
+            console.log(`Player ${player.name} wins`)
+            playerWin = true;
+        } else if(roundCount === 9){
+            console.log('Tie');
+        }
+    }
+
+    function checkRoundEnd(){
+        if(playerWin) return true;
+        if(roundCount === 9) return true;
+    }
+
+    function newRound (){
+        board.resetBoard();
+        roundCount = 0;
+        playerWin = false;
+    }
+
+    return {applyMark, newRound};
+
+})(playerA, playerB, gameBoard);
